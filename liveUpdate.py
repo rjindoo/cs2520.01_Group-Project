46634f7c2efda7e4f648c0ca565fef8de1375fb8
noahemiles <noahemiles@gmail.com>
2020-04-14 14:54:37 -0400

    constant feed test

---
+++ liveUpdate.py
@@ -0,0 +1,16 @@
+import urllib.request
+import os
+
+stockLabel = input("Enter Stock Label: ").upper()
+while True:
+	page = urllib.request.urlopen("http://www.finance.yahoo.com/quote/"+stockLabel)
+	pageText = page.read()
+	decodedPageText = pageText.decode("utf-8")
+	query = "<span class="
+	find = lambda sentence, qry: sentence[sentence.find(qry):sentence.find(qry)+97] if qry in sentence else -1
+
+	htmlLineWithPrice = find(decodedPageText, query)
+	#parse htmlLineWithPrice to only have price
+	stockPrice = htmlLineWithPrice.split(" ")[-1].split(">")[1].split("<")[0]
+	os.system('cls' if os.name == 'nt' else 'clear')
+	print(stockLabel + ':', stockPrice)
